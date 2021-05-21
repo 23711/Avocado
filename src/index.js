20de08080e7b8e32585377807f8a600b14929e51
Fernando Ochoa <Fernando268a@gmail.com>
2021-05-21 17:35:10 -0400

    Done but needs to be fix

--- src/index.js
+++ src/index.js
@@ -1,6 +1,48 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
 
-console.log('Happy hacking :)')
+// const url = "http://platzi-avo.vercel.app/api/avo";
+const baseUrl = "http://platzi-avo.vercel.app"
+const appNode = document.querySelector("div#container")
+
+const formatPrice = (price) => {
+    //https://developer.mozilla.org/en-US/docs/web/Javascript/Reference/Global_Objects/Intl
+    const newPrice = new window.Intl.NumberFormat("en-EN", {
+        style: "currency",
+        currency: "USD",
+    }).format(price)
+
+    return newPrice
+}
+
+//intl
+
+
+
+//web api
+//
+window.fetch(`${baseUrl}/api/avo`)
+    .then(response => { return response.json() })
+    .then(response =>{
+        console.log(response);
+        const elements = [];
+        response.data.forEach(item => {
+            //creat IMG
+            const image = document.createElement("img");
+            image.src = `${baseUrl}${item.image}`
+            image.className = "img"
+            //creat title
+            const title = document.createElement("h2");
+            title.textContent = item.name;
+            // title.style.fontSize = "3rem"
+            title.className = "so-big"
+            //creat price 
+            const price = document.createElement("p");
+            price.textContent = formatPrice(item.price);
+
+            //creat container
+            const container = document.createElement("div");
+            container.append(image, title, price);
+            container.className = "container"
+            elements.push(container)
+        })
+        appNode.append(...elements);
+    })
